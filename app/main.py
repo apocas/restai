e2161d49fb79c77125b5e118ff20fd2a1bcad0ac
Pedro Dias <petermdias@gmail.com>
2023-05-20 23:55:36 +0100

    try catch

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ if "PROJECTS_PATH" not in os.environ:
 app = FastAPI()
 brain = Brain()
 
+
 @app.get("/")
 async def get(request: Request):
     return "REST AI API, so many 'A's and 'I's, so little time..."
@@ -31,21 +32,28 @@ async def get(request: Request):
 async def getProjects(request: Request):
     return {"projects": brain.listProjects()}
 
+
 @app.get("/projects/{projectName}")
 async def getProject(projectName: str):
     try:
-      project = brain.loadProject(projectName)
-      dbInfo = project.db.get()
+        project = brain.loadProject(projectName)
+        dbInfo = project.db.get()
 
-      return {"project": project.model.name, "embeddings": project.model.embeddings, "documents": len(dbInfo["documents"]), "metadatas": len(dbInfo["metadatas"])}
+        return {"project": project.model.name, "embeddings": project.model.embeddings, "documents": len(dbInfo["documents"]), "metadatas": len(dbInfo["metadatas"])}
     except Exception as e:
-      raise HTTPException(
-          status_code=404, detail='{"error": ' + str(e) + '}')
+        raise HTTPException(
+            status_code=404, detail='{"error": ' + str(e) + '}')
+
 
 @app.delete("/projects/{projectName}")
 async def deleteProject(projectName: str):
-    brain.deleteProject(projectName)
-    return {"project": projectName}
+    try:
+        brain.deleteProject(projectName)
+        return {"project": projectName}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail='{"error": ' + str(e) + '}')
+
 
 @app.post("/projects")
 async def createProject(projectModel: ProjectModel):
@@ -59,44 +67,51 @@ async def createProject(projectModel: ProjectModel):
 
 @app.post("/projects/{projectName}/ingest/url")
 def ingestURL(projectName: str, ingest: IngestModel):
-    project = brain.loadProject(projectName)
+    try:
+        project = brain.loadProject(projectName)
 
-    loader = WebBaseLoader(ingest.url)
-    documents = loader.load()
+        loader = WebBaseLoader(ingest.url)
+        documents = loader.load()
 
-    texts = IndexDocuments(brain, project, documents)
-    project.db.persist()
+        texts = IndexDocuments(brain, project, documents)
+        project.db.persist()
 
-    return {"url": ingest.url, "texts": len(texts), "documents": len(documents)}
+        return {"url": ingest.url, "texts": len(texts), "documents": len(documents)}
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail='{"error": ' + str(e) + '}')
 
 
 @app.post("/projects/{projectName}/ingest/upload")
 def ingestFile(projectName: str, file: UploadFile):
-    project = brain.loadProject(projectName)
-
-    temp = NamedTemporaryFile(delete=False)
     try:
-        try:
-            contents = file.file.read()
-            with temp as f:
-                f.write(contents)
-        except Exception:
-            raise HTTPException(
-                status_code=500, detail='{"error": "Error while saving file."}')
-        finally:
-            file.file.close()
+        project = brain.loadProject(projectName)
 
-        _, ext = os.path.splitext(file.filename or '')
-        loader = FindFileLoader(temp, ext)
-        documents = loader.load()
+        temp = NamedTemporaryFile(delete=False)
+        try:
+            try:
+                contents = file.file.read()
+                with temp as f:
+                    f.write(contents)
+            except Exception:
+                raise HTTPException(
+                    status_code=500, detail='{"error": "Error while saving file."}')
+            finally:
+                file.file.close()
+
+            _, ext = os.path.splitext(file.filename or '')
+            loader = FindFileLoader(temp, ext)
+            documents = loader.load()
+        except Exception as e:
+            raise HTTPException(
+                status_code=500, detail='{"error": "Something went wrong."}')
+        finally:
+            os.remove(temp.name)
+
+        texts = IndexDocuments(brain, project, documents)
+        project.db.persist()
+
+        return {"filename": file.filename, "type": file.content_type, "texts": len(texts), "documents": len(documents)}
     except Exception as e:
         raise HTTPException(
-            status_code=500, detail='{"error": "Something went wrong."}')
-    finally:
-        os.remove(temp.name)
-
-    texts = IndexDocuments(brain, project, documents)
-    project.db.persist()
-
-    return {"filename": file.filename, "type": file.content_type, "texts": len(texts), "documents": len(documents)}
-
+            status_code=500, detail='{"error": ' + str(e) + '}')
