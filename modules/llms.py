23ff523ee47802cdeb89f56c5d28fe40560357b5
Pedro Dias <petermdias@gmail.com>
2023-11-07 01:38:23 +0000

    gpt4 turbo

--- modules/llms.py
+++ modules/llms.py
@@ -7,4 +7,5 @@ LLMS = {
     "gpt4all": (GPT4All, {"model": "./models/ggml-gpt4all-j-v1.3-groovy.bin", "backend": "gptj", "n_ctx": 1000}),
     "chat": (ChatOpenAI, {"temperature": 0, "model_name":"gpt-3.5-turbo"}),
     "chat4": (ChatOpenAI, {"temperature": 0, "model_name":"gpt-4"}),
+    "chat4_turbo": (ChatOpenAI, {"temperature": 0, "model_name":"gpt-4-1106-preview"}),
 }
