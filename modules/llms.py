4315b0dd2e2b78e0075047d054b7b4077ce83115
Pedro Dias <petermdias@gmail.com>
2023-12-05 15:57:41 +0000

    transformers pipeline

--- modules/llms.py
+++ modules/llms.py
@@ -1,5 +1,6 @@
 from langchain.llms import GPT4All, LlamaCpp, OpenAI
 from langchain.chat_models import ChatOpenAI
+from app.loader import localLoader
 
 LLMS = {
     "openai": (OpenAI, {"temperature": 0, "model_name": "text-davinci-003"}),
@@ -8,4 +9,5 @@ LLMS = {
     "openai_gpt4": (ChatOpenAI, {"temperature": 0, "model_name": "gpt-4"}),
     "openai_gpt4_turbo": (ChatOpenAI, {"temperature": 0, "model_name": "gpt-4-1106-preview"}),
     "llama13b_chat_gptq": (ChatOpenAI, {"temperature": 0.3, "openai_api_key": "na", "openai_api_base": "http://127.0.0.1:5000/v1"}),
+    "mistral7b_gptq": (localLoader, {"type": "gptq", "model": "TheBloke/Mistral-7B-OpenOrca-GPTQ"}),
 }
