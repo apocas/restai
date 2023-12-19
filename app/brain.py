d5ef5ad0b8e799c097ec745994fea5c36b3b9b12
Pedro Dias <petermdias@gmail.com>
2023-12-19 03:11:28 +0000

    cache

--- app/brain.py
+++ app/brain.py
@@ -380,6 +380,7 @@ class Brain:
         output = model.llm.llavaInference(input, visionInput.image)
         
         if loaded == True:
+            del self.llmCache[project.model.llm]
             self.semaphore.release()
             
         return output, [], 
