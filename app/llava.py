f6e99c89a5cb44733e1283853f28544923bebdcd
Pedro Dias <petermdias@gmail.com>
2023-12-19 02:12:14 +0000

    vision

--- app/llava.py
+++ app/llava.py
@@ -1,33 +1,40 @@
 import base64
+import gc
 from io import BytesIO
 import requests
 from PIL import Image
 
 class LlavaLLM:
-    def __init__(self, model):
-        self.model = model
+    def __init__(self, modelid):
+        self.modelid = modelid
     
     def llavaInference(self, prompt, imageb64):
         import torch
         from transformers import AutoProcessor, LlavaForConditionalGeneration
         
         model = LlavaForConditionalGeneration.from_pretrained(
-            self.model,
+            self.modelid,
             torch_dtype=torch.float16,
             low_cpu_mem_usage=True,
             load_in_4bit=True,
             use_flash_attention_2=True
         )
 
-        processor = AutoProcessor.from_pretrained(self.model)
+        processor = AutoProcessor.from_pretrained(self.modelid)
 
         raw_image = Image.open(BytesIO(base64.b64decode(imageb64)))
         
         inputs = processor(prompt, raw_image, return_tensors='pt').to(0, torch.float16)
 
-        output = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+        output_tensor = model.generate(**inputs, max_new_tokens=200, do_sample=False)
+        output = processor.decode(output_tensor[0][2:], skip_special_tokens=True)
         
-        return processor.decode(output[0][2:], skip_special_tokens=True)
+        model = None
+        del model
+        gc.collect()
+        torch.cuda.empty_cache()
+        
+        return output
 
 
 
