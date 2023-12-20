65d2ad375654b21d9e908765fba1d03ee01d0ba4
Pedro Dias <petermdias@gmail.com>
2023-12-20 13:06:47 +0000

    self

--- app/llm_tools.py
+++ app/llm_tools.py
@@ -39,45 +39,45 @@ class DalleImage(BaseTool):
         raise NotImplementedError("N/A")
 
 
+def sd_worker(prompt, sharedmem):
+    base = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    )
+    base.to("cuda")
+
+    refiner = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0",
+        text_encoder_2=base.text_encoder_2,
+        vae=base.vae,
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    )
+    refiner.to("cuda")
+
+    image = base(
+        prompt=prompt,
+        num_inference_steps=40,
+        denoising_end=0.8,
+        output_type="latent",
+    ).images
+    image = refiner(
+        prompt=prompt,
+        num_inference_steps=40,
+        denoising_start=0.8,
+        image=image,
+    ).images[0]
+
+    image_data = io.BytesIO()
+    image.save(image_data, format="JPEG")
+    image_base64 = base64.b64encode(image_data.getvalue()).decode('utf-8')
+
+    sharedmem["image"] = image_base64
+
 class StableDiffusionImage(BaseTool):
     name = "Stable Diffusion Image Generator"
     description = "use this tool when you need to generate an image using Stable Diffusion."
     return_direct = True
-    
-    def sd_worker(prompt, sharedmem):
-        base = DiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-        )
-        base.to("cuda")
-
-        refiner = DiffusionPipeline.from_pretrained(
-            "stabilityai/stable-diffusion-xl-refiner-1.0",
-            text_encoder_2=base.text_encoder_2,
-            vae=base.vae,
-            torch_dtype=torch.float16,
-            use_safetensors=True,
-            variant="fp16",
-        )
-        refiner.to("cuda")
-
-        image = base(
-            prompt=prompt,
-            num_inference_steps=40,
-            denoising_end=0.8,
-            output_type="latent",
-        ).images
-        image = refiner(
-            prompt=prompt,
-            num_inference_steps=40,
-            denoising_start=0.8,
-            image=image,
-        ).images[0]
-
-        image_data = io.BytesIO()
-        image.save(image_data, format="JPEG")
-        image_base64 = base64.b64encode(image_data.getvalue()).decode('utf-8')
-
-        sharedmem["image"] = image_base64
 
     def _run(self, query: str) -> str:
         llm = OpenAI(temperature=0.9)
@@ -92,7 +92,7 @@ class StableDiffusionImage(BaseTool):
         manager = multiprocessing.Manager()
         sharedmem = manager.dict()
 
-        p = multiprocessing.Process(target=self.sd_worker, args=(fprompt, sharedmem))
+        p = multiprocessing.Process(target=sd_worker, args=(fprompt, sharedmem))
         p.start()
         p.join()
 
