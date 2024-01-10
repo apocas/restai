b8c68858049c1a54f3a326cebb1818eeed2b4678
Pedro Dias <petermdias@gmail.com>
2024-01-10 20:44:48 +0000

    model details

--- modules/embeddings.py
+++ modules/embeddings.py
@@ -3,6 +3,6 @@ from langchain.embeddings import OpenAIEmbeddings, VertexAIEmbeddings, HuggingFa
 EMBEDDINGS = {
     "openai": (OpenAIEmbeddings, {}, "public", "https://platform.openai.com/docs/guides/embeddings"),
     "google_vertexai": (VertexAIEmbeddings, {}, "public", "https://cloud.google.com/vertex-ai/docs/generative-ai/learn/overview"),
-    "all-mpnet-base-v2": (HuggingFaceEmbeddings, {"model_name": "all-mpnet-base-v2"}, "private", "all-mpnet-base-v2 - https://www.sbert.net/docs/pretrained_models.html"),
-    "paraphrase-multilingual-mpnet-base-v2": (HuggingFaceEmbeddings, {"model_name": "paraphrase-multilingual-mpnet-base-v2"}, "private", "paraphrase-multilingual-mpnet-base-v2 - https://www.sbert.net/docs/pretrained_models.html"),
+    "all-mpnet-base-v2": (HuggingFaceEmbeddings, {"model_name": "all-mpnet-base-v2"}, "private", "all-mpnet-base-v2 - https://www.sbert.net/docs/pretrained_models.html - https://huggingface.co/sentence-transformers/all-mpnet-base-v2"),
+    "paraphrase-multilingual-mpnet-base-v2": (HuggingFaceEmbeddings, {"model_name": "paraphrase-multilingual-mpnet-base-v2"}, "private", "paraphrase-multilingual-mpnet-base-v2 - https://www.sbert.net/docs/pretrained_models.html - https://huggingface.co/sentence-transformers/paraphrase-multilingual-mpnet-base-v2"),
 }
