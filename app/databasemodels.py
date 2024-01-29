4fe384456d0fb15b22c9bec7c3063c256e83260b
Pedro Dias <petermdias@gmail.com>
2024-01-29 22:28:15 +0000

    system

--- app/databasemodels.py
+++ app/databasemodels.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -39,7 +39,7 @@ class ProjectDatabase(Base):
     embeddings = Column(String(255))
     type = Column(String(255))
     llm = Column(String(255))
-    system = Column(String(4096))
+    system = Column(Text)
     sandboxed = Column(Boolean, default=False)
     censorship = Column(String(4096))
     k = Column(Integer, default=4)
