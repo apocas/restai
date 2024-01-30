6629f4c806ff39f949c3365aee96406dc6e3e5bb
Pedro Dias <petermdias@gmail.com>
2024-01-30 10:25:06 +0000

    vectorstore default

--- app/databasemodels.py
+++ app/databasemodels.py
@@ -44,7 +44,7 @@ class ProjectDatabase(Base):
     censorship = Column(String(4096))
     k = Column(Integer, default=4)
     score = Column(Float, default=0.3)
-    vectorstore = Column(String(255), default="chroma")
+    vectorstore = Column(String(255))
     connection = Column(String(4096))
 
     owners = relationship("UserProjectDatabase", back_populates="project")
