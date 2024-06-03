3ca309c997c50b493b3625c3dc3971f110bf621c
Pedro Dias <petermdias@gmail.com>
2024-06-03 21:40:39 +0000

    public token

--- app/llms/tools/domainverify.py
+++ app/llms/tools/domainverify.py
@@ -1,9 +1,10 @@
+import os
 import requests
 
 def domainverify(domain: str) -> bool:
     """Verify if a domain is available"""
     try:
-        headers = {"Authorization": "Basic c2l0ZUBwdGlzcC5wdDpiNmUyNDIxNTkxYzE5NDZhODVkOWViNmQ1N2I0MWNkYw=="}
+        headers = {"Authorization": "Basic " + os.environ.get("PTISP_API")}
         response = requests.get(f"https://api.ptisp.pt/domains/{domain}/check", headers=headers)
         data = response.json()
         return data['available']
