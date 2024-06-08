2eba0b79fa08d597fc1aea34dd1f3488c0f7c781
Pedro Dias <petermdias@gmail.com>
2024-06-08 19:03:42 +0000

    sys

--- app/main.py
+++ app/main.py
@@ -431,21 +431,21 @@ async def get_project(projectName: str, user: User = Depends(get_current_usernam
             final_output["k"] = output["k"]
             final_output["score"] = output["score"]
             final_output["vectorstore"] = output["vectorstore"]
-            final_output["system"] = output["system"]
+            final_output["system"] = output["system"] or ""
             final_output["llm_rerank"] = output["llm_rerank"]
             final_output["colbert_rerank"] = output["colbert_rerank"]
             final_output["cache"] = output["cache"]
             final_output["cache_threshold"] = output["cache_threshold"]
         
         if project.model.type == "inference":
-            final_output["system"] = output["system"]
+            final_output["system"] = output["system"] or ""
             
         if project.model.type == "agent":
-            final_output["system"] = output["system"]
+            final_output["system"] = output["system"] or ""
             final_output["tools"] = output["tools"]
             
         if project.model.type == "ragsql":
-            final_output["system"] = output["system"]
+            final_output["system"] = output["system"] or ""
             final_output["tables"] = output["tables"]
             if output["connection"] is not None:
                 final_output["connection"] = re.sub(
