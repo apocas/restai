9f885d7f34aa26805e48719e23e123b3b2610f8a
Pedro Dias <petermdias@gmail.com>
2024-10-09 16:07:35 +0100

    crawler2

---
+++ app/llms/tools/crawler2.py
@@ -0,0 +1,18 @@
+from typing import Any, Dict
+
+import requests
+from bs4 import BeautifulSoup
+
+def crawler2(
+    url: str
+) -> str:
+    """
+    Retrieve a web page content. Useful to get content from a specific URL.
+
+    Args:
+        url (str): URL to be crawled.
+    """
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup.get_text()
