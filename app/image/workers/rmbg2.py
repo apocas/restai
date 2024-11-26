454ec61f96caa0df05347b3fccf5153f9114a102
Pedro Dias <petermdias@gmail.com>
2024-11-26 20:18:25 +0000

    rmbg2

---
+++ app/image/workers/rmbg2.py
@@ -0,0 +1,47 @@
+import base64
+import io
+import os
+from diffusers import StableDiffusion3Pipeline
+import torch
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+from torchvision import transforms
+from transformers import AutoModelForImageSegmentation
+
+from app.config import RESTAI_DEFAULT_DEVICE
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
+
+def worker(prompt, sharedmem):
+    img_data = base64.b64decode(sharedmem["input_image"])
+    input_image = Image.open(io.BytesIO(img_data))
+    
+    
+    model = AutoModelForImageSegmentation.from_pretrained('briaai/RMBG-2.0', trust_remote_code=True)
+    torch.set_float32_matmul_precision(['high', 'highest'][0])
+    model.to('cuda')
+    model.eval()
+
+    image_size = (1024, 1024)
+    transform_image = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
+    input_images = transform_image(input_image).unsqueeze(0).to('cuda')
+
+
+    with torch.no_grad():
+        preds = model(input_images)[-1].sigmoid().cpu()
+    pred = preds[0].squeeze()
+    pred_pil = transforms.ToPILImage()(pred)
+    mask = pred_pil.resize(input_image.size)
+    input_image.putalpha(mask)
+    
+    image_data = io.BytesIO()
+    input_image.save(image_data, format="PNG")
+    image_base64 = base64.b64encode(image_data.getvalue()).decode('utf-8')
+
+    sharedmem["image"] = image_base64
