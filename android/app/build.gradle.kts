ab118c03afc08674540bf95be12b6a79c293faf8
Pedro Dias <petermdias@gmail.com>
2026-05-24 11:51:28 +0100

    android version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cloud.restai.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.2.0"
     }
 
     buildTypes {
